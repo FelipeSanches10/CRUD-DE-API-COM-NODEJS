3aa88865fc825026791cdd9ae59166d651306fe2
Felipe Sanches <104999318+FelipeSanches10@users.noreply.github.com>
2024-02-17 12:35:26 -0300

    CRUD

---
+++ src/mocks/users.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'user_1',
+    },
+
+    {
+        id: 2,
+        name: 'user_2',
+    },
+
+    {
+        id: 3,
+        name: 'user_3',
+    },
+];
